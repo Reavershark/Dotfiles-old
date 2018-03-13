a56df9a281a5481d806eb3e3bb7dc75a32b935e8
Reavershark <jonasty1999@gmail.com>
2018-03-13 22:44:56 +0100

    Nvidia compton nouveau

--- .polybar/compton.sh
+++ .polybar/compton.sh
@@ -3,5 +3,5 @@
 if [ "$(pgrep -x compton)" ]; then
     pkill compton
 else
-    compton -b --config ~/.compton.conf
+    compton -b --config ~/.compton.conf --paint-on-overlay --unredir-if-possible --backend xr_glx_hybrid --vsync drm --glx-swap-method -1 --glx-no-stencil
 fi
